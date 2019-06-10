233bfdf72ee915d0ab9e9eeaab5e3c70913e9f23
stefian <v_ianta@yahoo.com>
2019-06-10 08:00:09 -0400

    added x and y scales linked to index of data and max value

--- 03-creating-charts/results.js
+++ 03-creating-charts/results.js
@@ -11,6 +11,13 @@ var svg = d3.select("#results")
   .attr("height", height)
 ;
 
+var x = d3.scale
+    .ordinal()
+    .rangeRoundBands([margin.left, width - margin.right], 0.1);
+var y = d3.scale
+    .linear()
+    .range([height - margin.bottom, margin.top]);
+
 /* Our standard data reloading function */
 var reload = function() {
   d3.csv("afcw-results.csv", function(rows) {
@@ -20,6 +27,9 @@ var reload = function() {
 
 /* Our standard graph drawing function */
 var redraw = function(data) {
+    x.domain(data.map(function (d,i) { return i; }));
+    y.domain([0, d3.max(data, function (d) { return d.GoalsScored })]);
+
     var bars = svg.selectAll("rect.bar")
         .data(data);
 
@@ -28,12 +38,10 @@ var redraw = function(data) {
         .classed("bar", true);
 
     bars
-        .attr("x", function(d, i) { return i * 6; })
+        .attr("x", function(d, i) { return x(i); })
         .attr("width", 5)
-        .attr("y", function(d) {
-            return height - margin.bottom - (d.GoalsScored * 50);
-        })
-        .attr("height", function(d) { return d.GoalsScored * 50 });
+        .attr("y", function(d) { return y(d.GoalsScored); })
+        .attr("height", function(d) { return y(0) - y(d.GoalsScored); });
 };
 
 reload();
