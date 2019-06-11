027063f90fd527a8f6b57a49ecea6f614f8f6538
stefian <v_ianta@yahoo.com>
2019-06-11 18:10:52 -0400

    read stats.tsv and filtered, mapped, sorted and sliced top 100

--- 06-third-parties/medley.js
+++ 06-third-parties/medley.js
@@ -2,38 +2,47 @@
 var width = 750,
     height = 500;
 
-
-
-// script below from https://github.com/jasondavies/d3-cloud/blob/v1.0.5/examples/simple.html
 var fill = d3.scale.category20();
-d3.layout.cloud().size([width, height])
-    .words([
-      "Hello", "world", "normally", "you", "want", "more", "words",
-      "than", "this"].map(function(d) {
-      return {text: d, size: 10 + Math.random() * 90};
-    }))
-    .padding(5)
-    .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .font("Impact")
-    .fontSize(function(d) { return d.size; })
-    .on("end", draw)
-    .start();
+
+d3.tsv("stats.tsv", function (data) {
+    // read stats, 
+    // filter for records with G> 0, 
+    // map to new records with text and size - as required by d3 word cloud
+    // sort descending with top names / size=Goals
+    // slice to keep the first/top 100 names
+    var leaders = data
+        .filter(function (d) { return +d.G > 0; })
+        .map(function (d) { return { text: d.Name, size: +d.G }; })
+        .sort(function (a, b) { return d3.descending(a.size, b.size); })
+        .slice(0, 100);
+
+    // script below from https://github.com/jasondavies/d3-cloud/blob/v1.0.5/examples/simple.html
+    d3.layout.cloud().size([width, height])
+        .words(leaders)
+        .padding(5)
+        .rotate(function () { return ~~(Math.random() * 2) * 90; })
+        .font("Impact")
+        .fontSize(function (d) { return d.size; })
+        .on("end", draw)
+        .start();
+
+})
 
 function draw(words) {
-  d3.select("#word-cloud").append("svg")
-      .attr("width", width)
-      .attr("height", height)
-    .append("g")
-      .attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")")
-    .selectAll("text")
-      .data(words)
-    .enter().append("text")
-      .style("font-size", function(d) { return d.size + "px"; })
-      .style("font-family", "Impact")
-      .style("fill", function(d, i) { return fill(i); })
-      .attr("text-anchor", "middle")
-      .attr("transform", function(d) {
-        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-      })
-      .text(function(d) { return d.text; });
+    d3.select("#word-cloud").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + (width / 2) + "," + (height / 2) + ")")
+        .selectAll("text")
+        .data(words)
+        .enter().append("text")
+        .style("font-size", function (d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .style("fill", function (d, i) { return fill(i); })
+        .attr("text-anchor", "middle")
+        .attr("transform", function (d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function (d) { return d.text; });
 }
