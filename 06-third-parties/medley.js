2406412e17da05c4311326c224554f05632d877f
stefian <v_ianta@yahoo.com>
2019-06-11 20:16:16 -0400

    included and adapted sunburst partition code

--- 06-third-parties/medley.js
+++ 06-third-parties/medley.js
@@ -1,10 +1,37 @@
 
 var width = 750,
-    height = 500;
+    height = 500,
+    radius = Math.min(width, height) / 2;
 
 var leaderScale = d3.scale.linear().range([10, 60]);
 
 var fill = d3.scale.category20();
+
+// START - from Sunburst Partition: https://gist.github.com/mbostock/4063423
+var sunburst = d3.select("#top-scorers").append("svg")
+    .attr("width", width)
+    .attr("height", height * 1.04)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height * .52 + ")");
+
+var partition = d3.layout.partition()
+    .sort(null)
+    .size([2 * Math.PI, radius * radius])
+    .value(function (d) { return 1; })
+    .children(function (d) {
+        console.log(d);
+        return d.children ? d.children :
+            d.entries ? d.entries() :
+                d.text ? null :
+                    d.value.length ? d.value : d.value.entries();
+    });
+
+var arc = d3.svg.arc()
+    .startAngle(function (d) { return d.x; })
+    .endAngle(function (d) { return d.x + d.dx; })
+    .innerRadius(function (d) { return Math.sqrt(d.y); })
+    .outerRadius(function (d) { return Math.sqrt(d.y + d.dy); });
+// END - from Sunburst Partition: https://gist.github.com/mbostock/4063423
 
 d3.tsv("stats.tsv", function (data) {
     // read stats, 
@@ -23,6 +50,11 @@ d3.tsv("stats.tsv", function (data) {
         .sort(function (a, b) { return d3.descending(a.size, b.size); })
         .slice(0, 100);
 
+    var leadersByTeamPos = d3.nest()
+        .key(d => { return d.team; })
+        .key(d => { return d.pos; })
+        .map(leaders, d3.map);
+
     leaderScale
         .domain([d3.min(leaders, d => { return d.size; }),
         d3.max(leaders, d => { return d.size })
@@ -38,7 +70,8 @@ d3.tsv("stats.tsv", function (data) {
         .on("end", drawCloud)
         .start();
 
-})
+    drawSunburst(leadersByTeamPos);
+});
 
 function drawCloud(words) {
     d3.select("#word-cloud").append("svg")
@@ -57,4 +90,44 @@ function drawCloud(words) {
             return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
         })
         .text(function (d) { return d.text; });
+}
+
+function drawSunburst(data) {
+    var path = sunburst.datum(data).selectAll("path")
+        .data(partition.nodes)
+        .enter().append("path")
+        .attr("display", function (d) { return d.depth ? null : "none"; }) // hide inner ring
+        .attr("d", arc)
+        .style("stroke", "#fff")
+        .style("fill", function (d) { return fill(d.children ? d.key : d.text); })
+        .style("fill-rule", "evenodd")
+        .each(stash);
+
+    d3.selectAll("input").on("change", function change() {
+        var value = this.value === "count"
+            ? function () { return 1; }
+            : function (d) { return d.goals; };
+
+        path
+            .data(partition.value(value).nodes)
+            .transition()
+            .duration(1500)
+            .attrTween("d", arcTween);
+    });
+}
+
+// Stash the old values for transition.
+function stash(d) {
+    d.x0 = d.x;
+    d.dx0 = d.dx;
+}
+// Interpolate the arcs in data space.
+function arcTween(a) {
+    var i = d3.interpolate({ x: a.x0, dx: a.dx0 }, a);
+    return function (t) {
+        var b = i(t);
+        a.x0 = b.x;
+        a.dx0 = b.dx;
+        return arc(b);
+    };
 }
